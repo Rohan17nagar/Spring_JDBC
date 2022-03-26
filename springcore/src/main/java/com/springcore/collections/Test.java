cb58f4b1b5d61038f9b60e4feadfb77b65e03b13
Rohan Nagar <46292211+Rohan17nagar@users.noreply.github.com>
2022-03-26 17:25:53 +0530

    changing from github directly.

--- springcore/src/main/java/com/springcore/collections/Test.java
+++ springcore/src/main/java/com/springcore/collections/Test.java
@@ -13,5 +13,7 @@ public class Test {
 	System.out.println(emp1.getCourses());
 	System.out.println(emp1.getAddresses());
 	System.out.println(emp1.getPhones().getClass().getName());
+		System.out.println("hello");
 	}
+	
 }
