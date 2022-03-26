65b998efc881bd70e982ea5996f928380c110c45
rohan <rohan@Rohan>
2022-03-26 18:12:03 +0530

    javaconfig with annotations.

--- springcore/src/main/java/com/springcore/javaconfig/Student.java
+++ springcore/src/main/java/com/springcore/javaconfig/Student.java
@@ -1,5 +1,26 @@
 package com.springcore.javaconfig;
 
-public class Student {
+import org.springframework.stereotype.Component;
 
+public class Student {
+	
+	private Samosa samosa;
+	
+	public Student(Samosa samosa) {
+		super();
+		this.samosa = samosa;
+	}
+
+	public Samosa getSamosa() {
+		return samosa;
+	}
+
+	public void setSamosa(Samosa samosa) {
+		this.samosa = samosa;
+	}
+
+	public void study() {
+		this.samosa.display();
+		System.out.println("student is reading book.::Student Class");
+	}
 }
--- springcore/src/main/java/com/springcore/javaconfig/Test.java
+++ springcore/src/main/java/com/springcore/javaconfig/Test.java
@@ -1,10 +1,16 @@
 package com.springcore.javaconfig;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test {
 
 	public static void main(String[] args) {
-		
-
+		ApplicationContext context = new AnnotationConfigApplicationContext(JavaConfig.class);
+		Student student1 = context.getBean("getStudent", Student.class);
+		System.out.println(student1);
+		student1.study();
 	}
 
 }
